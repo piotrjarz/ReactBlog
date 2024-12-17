dcce194ec122d31c0175804d044df2cd1a65f041
unknown <p.jarzembski@op.pl>
2024-12-17 12:30:48 +0100

    Changed nav bg color to dark

--- src/pages/shared/Nav.jsx
+++ src/pages/shared/Nav.jsx
@@ -24,70 +24,48 @@ const WebNav = () =>{
     }
 
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-            <img src={logo} alt="Logo" style={{width: "60px", height: "60px"}}/>
-        <Navbar.Brand href="/">Goju Blog</Navbar.Brand>
-            <Row>
-                
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Strona główna</Nav.Link>
-                    <Nav.Link href="/posts">Aktualności</Nav.Link>
-                </Nav>
-                {
-                    (admin === 'true') ? (
-                        <Nav className="me-auto">
-                            <NavDropdown title="Wpisy" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/posts/mine">
-                                Twoje wpisy
-                                </NavDropdown.Item>
+        <Navbar expand="lg" className="bg-dark text-light">
+  <Container>
+    <img src={logo} alt="Logo" style={{ width: "60px", height: "60px" }} />
+    <Navbar.Brand href="/">Goju Blog</Navbar.Brand>
+    <Row>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/" className="text-light">Strona główna</Nav.Link>
+          <Nav.Link href="/posts" className="text-light">Aktualności</Nav.Link>
+        </Nav>
+        {admin === 'true' && (
+          <Nav className="me-auto">
+            <NavDropdown title="Wpisy" id="basic-nav-dropdown" className="text-light">
+              <NavDropdown.Item href="/posts/mine" className="text-dark">Twoje wpisy</NavDropdown.Item>
+              <NavDropdown.Item href="/posts/add" className="text-dark">Dodaj wpis</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        )}
+      </Navbar.Collapse>
+    </Row>
 
-                                <NavDropdown.Item href="/posts/add">
-                                Dodaj wpis
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    ) : (<></>)
-                }
-                
+    <Row>
+      <Nav>
+        <NavDropdown title="Twoje konto" id="basic-nav-dropdown-1" className="text-light" style={{color: 'white'}}>
+          {user ? (
+            <>
+              <NavDropdown.Item href="/profile" className="text-dark">Profil</NavDropdown.Item>
+              <NavDropdown.Item href="/login" onClick={handleLougout} className="text-light">Wyloguj się</NavDropdown.Item>
+            </>
+          ) : (
+            <>
+              <NavDropdown.Item href="/login" className="text-dar">Zaloguj się</NavDropdown.Item>
+              <NavDropdown.Item href="/register" className="text-light">Zarejestruj się</NavDropdown.Item>
+            </>
+          )}
+        </NavDropdown>
+      </Nav>
+    </Row>
+  </Container>
+</Navbar>
 
-                </Navbar.Collapse>
-            </Row>
-
-            <Row>
-                <Nav>
-                    <NavDropdown title="Twoje konto" id="basic-nav-dropdown">
-                        {
-                            user ? (
-                                <>
-                                
-                                <NavDropdown.Item href="/profile">
-                                Profil
-                                </NavDropdown.Item>
-
-                                <NavDropdown.Item href="/login" onClick={handleLougout}>
-                                Wyloguj się
-                                </NavDropdown.Item>
-                                </>
-                            ) : (
-                            <>
-                                <NavDropdown.Item href="/login">
-                                Zaloguj się
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/register">
-                                Zarejestruj się
-                                </NavDropdown.Item>
-                            </>
-                            )
-                        }
-                        
-                    </NavDropdown>
-                </Nav>
-            </Row>
-        </Container>
-      </Navbar>
     )
 }
 
